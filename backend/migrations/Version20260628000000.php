64bb50ca1cff9256ca037a260e92ed5914861282
mlussier <ordi.media@hotmail.com>
2026-06-28 09:17:23 -0400

    Add unique indexes onto the user table for username and email.

---
+++ backend/migrations/Version20260628000000.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260628000000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Enforce unique usernames and email addresses';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_USER_USERNAME ON user (username)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_USER_EMAIL ON user (email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX UNIQ_USER_USERNAME');
+        $this->addSql('DROP INDEX UNIQ_USER_EMAIL');
+    }
+}
--- backend/src/Domain/User/User.php
+++ backend/src/Domain/User/User.php
@@ -15,6 +15,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'user')]
+#[ORM\UniqueConstraint(name: 'UNIQ_USER_USERNAME', columns: ['username'])]
+#[ORM\UniqueConstraint(name: 'UNIQ_USER_EMAIL', columns: ['email'])]
 #[ORM\HasLifecycleCallbacks]
 #[UniqueEntity(
     fields: ['username'],
