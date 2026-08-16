50f080e60cb05a65565933767f53ebf6e3435955
mlussier <ordi.media@hotmail.com>
2026-08-15 22:00:55 -0400

    Adapt migration script from SQLite to PostgreSQL

--- backend/migrations/Version20260205213401.php
+++ backend/migrations/Version20260205213401.php
@@ -19,17 +19,11 @@ final class Version20260205213401 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number FROM user');
-        $this->addSql('DROP TABLE user');
-        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, salutation VARCHAR(64) DEFAULT NULL, pronouns VARCHAR(64) DEFAULT NULL, gender_identity VARCHAR(64) DEFAULT NULL, first_name VARCHAR(128) NOT NULL, middle_name VARCHAR(128) DEFAULT NULL, last_name VARCHAR(128) NOT NULL, email VARCHAR(256) NOT NULL, phone_number VARCHAR(64) DEFAULT NULL)');
-        $this->addSql('INSERT INTO user (id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number) SELECT id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number FROM __temp__user');
-        $this->addSql('DROP TABLE __temp__user');
+        $this->addSql('ALTER TABLE "user" DROP password');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD COLUMN password VARCHAR(256) NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD password VARCHAR(256) NOT NULL');
     }
 }
--- backend/migrations/Version20260206145648.php
+++ backend/migrations/Version20260206145648.php
@@ -19,21 +19,13 @@ final class Version20260206145648 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number FROM user');
-        $this->addSql('DROP TABLE user');
-        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, salutation VARCHAR(64) DEFAULT NULL, pronouns VARCHAR(64) DEFAULT NULL, gender_identity VARCHAR(64) DEFAULT NULL, first_name VARCHAR(128) NOT NULL, middle_name VARCHAR(128) DEFAULT NULL, last_name VARCHAR(128) NOT NULL, email VARCHAR(256) NOT NULL, phone_number VARCHAR(64) DEFAULT NULL, created_at DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP), updated_at DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP))');
-        $this->addSql('INSERT INTO user (id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number, created_at, updated_at) SELECT id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP FROM __temp__user');
-        $this->addSql('DROP TABLE __temp__user');
-        $this->addSql('CREATE TRIGGER trig_user_au_timestamps AFTER UPDATE ON user FOR EACH ROW WHEN NEW.updated_at = OLD.updated_at BEGIN UPDATE user SET updated_at = CURRENT_TIMESTAMP WHERE id = NEW.id; END');
+        $this->addSql('ALTER TABLE "user" ADD created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TRIGGER IF EXISTS trig_user_au_timestamps');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number FROM user');
-        $this->addSql('DROP TABLE user');
-        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, salutation VARCHAR(64) DEFAULT NULL, pronouns VARCHAR(64) DEFAULT NULL, gender_identity VARCHAR(64) DEFAULT NULL, first_name VARCHAR(128) NOT NULL, middle_name VARCHAR(128) DEFAULT NULL, last_name VARCHAR(128) NOT NULL, email VARCHAR(256) NOT NULL, phone_number VARCHAR(64) DEFAULT NULL)');
-        $this->addSql('INSERT INTO user (id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number) SELECT id, salutation, pronouns, gender_identity, first_name, middle_name, last_name, email, phone_number FROM __temp__user');
-        $this->addSql('DROP TABLE __temp__user');
+        $this->addSql('ALTER TABLE "user" DROP updated_at');
+        $this->addSql('ALTER TABLE "user" DROP created_at');
     }
 }
